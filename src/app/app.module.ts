2f72c8d66c0ed1c0a5ac467c0abd0139988fc60d
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-05-08 15:01:35 +0900

    ng g c components/conversation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { ListComponent } from './components/list/list.component';
 import { TalkListComponent } from './pages/talk-list/talk-list.component';
 import { TalkComponent } from './pages/talk/talk.component';
+import { ConversationComponent } from './components/conversation/conversation.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ListComponent, TalkListComponent, TalkComponent],
+  declarations: [AppComponent, HeaderComponent, ListComponent, TalkListComponent, TalkComponent, ConversationComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
