b21b77f3dbb49339b4d75283e6e911ddc308f5d6
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-05-08 14:41:09 +0900

    sectionをクリックするとtalk pageへ遷移

--- src/app/pages/talk-list/talk-list.component.ts
+++ src/app/pages/talk-list/talk-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-talk-list',
   templateUrl: './talk-list.component.html',
-  styleUrls: ['./talk-list.component.scss']
+  styleUrls: ['./talk-list.component.scss'],
 })
 export class TalkListComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  gotoTalk(): void {
+    this.router.navigate(['talk']);
   }
-
 }
