ba77a63a6eb9c1f25a4c7ca2ae33deabd65ed9e0
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-05-08 13:21:40 +0900

    ヘッダーのテンプレートを実装

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  headerText: string;
+  faArrowLeft = faArrowLeft;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.headerText = 'さデラパッション';
   }
-
 }
