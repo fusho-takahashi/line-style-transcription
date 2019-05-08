dee2a8e1e1511c9e6f8d19b937388dd52445bcc5
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-05-08 14:53:29 +0900

    routerにtalk componentを登録

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TalkListComponent } from './pages/talk-list/talk-list.component';
+import { TalkComponent } from './pages/talk/talk.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/talk-list', pathMatch: 'full' },
   { path: 'talk-list', component: TalkListComponent },
+  { path: 'talk', component: TalkComponent },
 ];
 
 @NgModule({
